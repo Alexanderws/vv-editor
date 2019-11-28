2cbb7e42b7b37d1e37b5f7c2fcfcdb9e1c2d267e
alexanderws <Alex.ws@icloud.com>
2019-11-28 17:03:52 +0100

    Updates

---
+++ precache-manifest.b3d4ba9ee1ad170f7bfbf211b5990e9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0007c0f603e7578ea94191a93d3ca6ee",
+    "url": "/vv-editor/index.html"
+  },
+  {
+    "revision": "afc5c972e708533ca241",
+    "url": "/vv-editor/static/css/main.702fed02.chunk.css"
+  },
+  {
+    "revision": "d4c738addc6fee7fe08b",
+    "url": "/vv-editor/static/js/2.ad910900.chunk.js"
+  },
+  {
+    "revision": "afc5c972e708533ca241",
+    "url": "/vv-editor/static/js/main.3fe39ffe.chunk.js"
+  },
+  {
+    "revision": "7c12ff31a8f2335d4062",
+    "url": "/vv-editor/static/js/runtime-main.763c8d24.js"
+  }
+]);
